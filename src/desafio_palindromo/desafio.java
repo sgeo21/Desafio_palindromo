b24059beb2ccc7c0da53fb898ed0fb59af2af011
Geovana <s.geo2112@gmail.com>
2025-11-27 21:21:06 -0300

    :tada: commit inicial

---
+++ src/desafio_palindromo/desafio.java
@@ -0,0 +1,33 @@
+package desafio_palindromo;
+
+import java.util.Scanner;
+
+public class desafio {
+
+	public static void main(String[] args) {
+		
+		    Scanner scanner = new Scanner(System.in);
+
+		    System.out.print("Digite uma palavra ou número: ");
+		    String entrada = scanner.nextLine();
+
+		    if (isPalindromo(entrada)){
+		        System.out.println(" " + entrada + " é um palíndromo!");
+
+		    } else {
+		        System.out.println(" " + entrada + " não é um palídromo");
+		    } 
+		    scanner.close();
+		} 
+		    public static boolean isPalindromo(String entradas) {
+		        // 1. Converter para minúsculas para ignorar maiúsculas e minúsculas
+		        String entradaFormatada = entradas.toLowerCase();
+
+		        // 2. Inverter a string
+		        String entradaInvertida = new StringBuilder(entradaFormatada).reverse().toString();
+
+		        // 3. Comparar a string original (formatada) com a invertida
+		        return entradaFormatada.equals(entradaInvertida);
+		    }
+
+	}

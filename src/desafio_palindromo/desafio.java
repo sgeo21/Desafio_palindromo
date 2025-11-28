ce19ccee2869c38f0df63a6e2bfa8e44c17febab
Geovana <s.geo2112@gmail.com>
2025-11-27 21:48:43 -0300

    versão final

--- src/desafio_palindromo/desafio.java
+++ src/desafio_palindromo/desafio.java
@@ -1,9 +1,9 @@
 package desafio_palindromo;
 
+import java.text.Normalizer;
 import java.util.Scanner;
 
 public class desafio {
-
 	public static void main(String[] args) {
 		
 		    Scanner scanner = new Scanner(System.in);
@@ -22,6 +22,13 @@ public class desafio {
 		    public static boolean isPalindromo(String entradas) {
 		        // 1. Converter para minúsculas para ignorar maiúsculas e minúsculas
 		        String entradaFormatada = entradas.toLowerCase();
+		        
+		        // 2. Remove acentos
+		        entradaFormatada = Normalizer.normalize(entradaFormatada, Normalizer.Form.NFD)
+						.replaceAll("[\\p{InCombiningDiacriticalMarks}]", "");
+
+				// 3. Remove espaços e caracteres não alfanuméricos
+				entradaFormatada = entradaFormatada.replaceAll("[^a-z0-9]", "");
 
 		        // 2. Inverter a string
 		        String entradaInvertida = new StringBuilder(entradaFormatada).reverse().toString();
@@ -29,5 +36,4 @@ public class desafio {
 		        // 3. Comparar a string original (formatada) com a invertida
 		        return entradaFormatada.equals(entradaInvertida);
 		    }
-
 	}
